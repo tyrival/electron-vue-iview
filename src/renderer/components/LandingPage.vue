5dbf958a1ed25d2ee4d4c6248aef2f8fb1a2a78f
Tyrival <tyrival@qq.com>
2019-03-01 08:30:55 +0800

    初始化工程

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="text">Text</Button>
+    <br><br>
+    <Button type="info">Info</Button>
+    <Button type="success">Success</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="error">Error</Button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    methods: {
+    }
+  }
+</script>
---
+++ src/renderer/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+import iView from 'iview'
+import './assets/styles/theme/light.less'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+Vue.use(iView, {
+  // 全局设置组件默认尺寸
+  size: 'small'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  components: {App},
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
